e08d737df670df0d8bc5d3203d2a2e3c4707cbd0
diana <sandu.diana2001@gmail.com>
2023-11-29 15:00:39 +0200

    exercise 5

--- Module 1/Exercise_5.js
+++ Module 1/Exercise_5.js
@@ -3,9 +3,9 @@
 const input = prompt("Enter a year : ")
 let year = parseInt(input)
 
-if (year % 4 == 0) {
+if (year % 4 === 0) {
     document.querySelector('#result').innerHTML = 'The year ' + year + ' is a leap year.';
-} else if (year % 4 == 0 && year % 400 == 0) {
+} else if (year % 4 === 0 && year % 400 === 0) {
     document.querySelector('#result').innerHTML = 'The year ' + year + ' is a leap year.';
 } else {
     document.querySelector('#result').innerHTML = 'The year ' + year + ' is not a leap year.';
