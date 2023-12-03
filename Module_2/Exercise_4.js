03e96ab4aeccb73ed71d3efa34fff1d8ca1e73b8
diana <sandu.diana2001@gmail.com>
2023-12-03 16:42:31 +0200

    exercise 5

--- Module_2/Exercise_4.js
+++ Module_2/Exercise_4.js
@@ -6,15 +6,9 @@ while (input > 0 && !isNaN(input)) {
     input = parseInt(prompt("Enter a number"));
 }
 
-list.sort();
-list.reverse();
-
-/*
-// Compare numbers instead of strings
 list.sort(function(a, b) {
     return b - a;
 });
-*/
 
 console.log(list);
 
