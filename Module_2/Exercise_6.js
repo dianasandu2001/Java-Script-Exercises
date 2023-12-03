3bbe45813a21a81c9ebf2cee8180666b0221e3b9
diana <sandu.diana2001@gmail.com>
2023-12-03 16:59:00 +0200

    exercise 6

---
+++ Module_2/Exercise_6.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function dice_roll () {
+    return Math.floor(Math.random() * 6) + 1;
+}
+let dice = 1
+while (dice < 6) {
+    dice = dice_roll()
+    document.getElementById("dice").innerHTML += `<ul>${dice}</ul>`;
+}
